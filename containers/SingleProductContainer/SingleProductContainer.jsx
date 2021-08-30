725f3f7832c03918c5a1f27e50273ce1b191f506
Ismail-Lh <ismailsaad03@gmail.com>
2021-08-30 16:38:39 +0100

    Add the links to the singleProductPage

--- containers/SingleProductContainer/SingleProductContainer.jsx
+++ containers/SingleProductContainer/SingleProductContainer.jsx
@@ -4,7 +4,12 @@ import classes from './SingleProductContainer.module.scss';
 
 import { useProductsContext } from '../../contexts/products_context';
 
-import { SingleProductImages, SingleProductInfo } from '../../components';
+import {
+  Button,
+  MyLink,
+  SingleProductImages,
+  SingleProductInfo,
+} from '../../components';
 
 const ProductsSliderSection = dynamic(() =>
   import('../../sections/ProductsSliderSection/ProductsSliderSection')
@@ -16,10 +21,30 @@ const SingleProductContainer = () => {
 
   const [color, setColor] = useState(product?.productColor);
 
+  const { gender, collections, name } = product?.productInfo;
+
+  const genderCollection = gender === 'men' ? 'shop-men' : 'shop-women';
+
   return (
     <div className={classes.singleProduct}>
       <div className='container'>
-        <div className={classes.singleProduct_container}>
+        <div className={classes.singleProduct__links}>
+          <MyLink route='/'>Home /</MyLink>
+          <MyLink route={`/collections/${genderCollection}`}>
+            {genderCollection} /
+          </MyLink>
+          <MyLink route={`/collections/${collections}`}>{collections} /</MyLink>
+          <p>{name}</p>
+        </div>
+
+        <div className={classes.singleProduct__btns}>
+          <Button route='/'>Home page</Button>
+          <Button route={`/collections/${genderCollection}`}>
+            {genderCollection}
+          </Button>
+        </div>
+
+        <div className={classes.singleProduct__container}>
           <SingleProductImages product={product?.productInfo} color={color} />
           <SingleProductInfo
             product={product?.productInfo}
